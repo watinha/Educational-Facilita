82f2450f74ac7c802cb3c3880765847feb2d8011
Willian Massami Watanabe <watinha@gmail.com>
2010-01-14 22:48:31 -0200

    Updating the version of READABILITY module from version 0.4 to version 0.5.

--- educational_facilita.js
+++ educational_facilita.js
@@ -5,88 +5,30 @@
   *** Authors: Willian Massami Watanabe, Arnaldo Candido  ***
   ***          Jr., Marcelo Adriano Am‚ncio e Matheus de  ***
   ***          Oliveira                                   ***
+  *** Contact: watinha@gmail.com                          ***
+  *** Last changes: 14/01/2010                            ***
   ***********************************************************
 */
 /*
   Development log:
-    - Version 0.1: Readability Module for textual extraction of the site
+    - Version 0.1: Readability Module 0.5 for textual extraction of the site
 */
-
-var Textual_processing = {
-  readability_module: function(document_tab){
-    /*
-    *** Extracted from: http://lab.arc90.com/experiments/readability  ***
-    */
-    
-    var allParagraphs = document_tab.getElementsByTagName("p");
-    var topDiv = null;
-
-  	// Replace all doubled-up <BR> tags with <P> tags, and remove fonts.
-  	//var pattern =  new RegExp ("<br/?>[ \r\n\s]*<br/?>", "g");
-  	//document_tab.body.innerHTML = document_tab.body.innerHTML.replace(pattern, "</p><p>").replace(/<\/?font[^>]*>/g, '');
-
-	  // Grab the title from the <title> tag and inject it as the title.
-	  //articleTitle.innerHTML = document_tab.title;
-	  //articleContent.appendChild(articleTitle);
-	
-	  // Study all the paragraphs and find the chunk that has the best score.
-	  // A score is determined by things like: Number of <p>'s, commas, special classes, etc.
-	  for (var j=0; j	< allParagraphs.length; j++) {
-		  parentNode = allParagraphs[j].parentNode;
-
-		  // Initialize readability data
-		  if(typeof parentNode.readability == 'undefined')
-		  {
-			  parentNode.readability = {"contentScore": 0};			
-
-			  // Look for a special classname
-			  if(parentNode.className.match(/(comment|meta|footer|footnote)/))
-			  	parentNode.readability.contentScore -= 50;
-			  else if(parentNode.className.match(/((^|\\s)(post|hentry|entry[-]?(content|text|body)?|article[-]?(content|text|body)?)(\\s|$))/))
-				  parentNode.readability.contentScore += 25;
-
-		  	// Look for a special ID
-		  	if(parentNode.id.match(/(comment|meta|footer|footnote)/))
-		  		parentNode.readability.contentScore -= 50;
-		  	else if(parentNode.id.match(/^(post|hentry|entry[-]?(content|text|body)?|article[-]?(content|text|body)?)$/))
-		  		parentNode.readability.contentScore += 25;
-		  }
-
-		  // Add a point for the paragraph found
-		  if(allParagraphs[j].textContent.length > 10)
-		  	parentNode.readability.contentScore++;
-
-	  	// Add points for any commas within this paragraph
-	  	parentNode.readability.contentScore += allParagraphs[j].textContent.split(",").length;
-	  }
-
-	  // Assignment from index for performance. See http://www.peachpit.com/articles/article.aspx?p=31567&seqNum=5 
-  	for(nodeIndex = 0; (node = document_tab.getElementsByTagName('*')[nodeIndex]); nodeIndex++)
-  		if(typeof node.readability != 'undefined' && (topDiv == null || node.readability.contentScore > topDiv.readability.contentScore))
-	  		topDiv = node;
-    
-    return topDiv;
-  }, 
-  recover_text: function(element){
-    return $(element).text();    
-  }
-    
-};
 
 var Educational_Facilita = {
   version: 0.1,
   initialize: function(){
-    var main_element = Textual_processing.readability_module(jetpack.tabs.focused.contentDocument);
+    var main_element = readability.grabArticle(jetpack.tabs.focused.contentDocument);
     
     // finishes if there is no main element
     if(main_element == null){
-      jetpack.notifications.show("N„o h· texto suficiente para ser transformado nessa p·gina.")
+      jetpack.notifications.show("N√£o√° texto suficiente para ser processado nessa p√ina.");
       return 0;
     }
       
-    $(jetpack.tabs.focused.contentDocument.body).append(Textual_processing.recover_text(main_element));
+    $(jetpack.tabs.focused.contentDocument.body).append($(main_element).text());
   }
 };
+
 jetpack.statusBar.append({
   html: "<button style='background-color:#000000;color:#FFFFFF;text-align:center;padding: 0px 10px'>Educational</button>",  
   onReady: function(widget){
@@ -95,3 +37,389 @@ jetpack.statusBar.append({
     });    
   }
 });
+
+/*
+ * Readability. An Arc90 Lab Experiment. 
+ * Website: http://lab.arc90.com/experiments/readability
+ * Source:  http://code.google.com/p/arc90labs-readability
+ * 
+ *  *** CHANGED TO NOT REPLACE BUT ONLY FIND MAIN ELEMENT ***
+ *
+ * Copyright (c) 2009 Arc90 Inc
+ * Readability is licensed under the Apache License, Version 2.0.
+**/
+var dbg = function(s) {
+	if(typeof console !== 'undefined')
+		console.log("Readability: " + s);
+};
+var readability = {
+	version:     '0.5.1',
+	emailSrc:    'http://lab.arc90.com/experiments/readability/email.php',
+	kindleSrc:   'http://lab.arc90.com/experiments/readability/kindle.php',
+	iframeLoads: 0,
+	frameHack:   false, /**
+	                     * The frame hack is to workaround a firefox bug where if you
+						 * pull content out of a frame and stick it into the parent element, the scrollbar won't appear.
+						 * So we fake a scrollbar in the wrapping div.
+						**/
+	bodyCache:  null,   /* Cache the body HTML in case we need to re-use it later */
+	flags: 0x1 | 0x2,   /* Start with both flags set. */
+	
+	/* constants */
+	FLAG_STRIP_UNLIKELYS: 0x1,
+	FLAG_WEIGHT_CLASSES:  0x2,
+	
+	/**
+	 * All of the regular expressions in use within readability.
+	 * Defined up here so we don't instantiate them repeatedly in loops.
+	 **/
+	regexps: {
+		unlikelyCandidatesRe:   /combx|comment|disqus|foot|header|menu|meta|rss|shoutbox|sidebar|sponsor/i,
+		okMaybeItsACandidateRe: /and|article|body|column|main/i,
+		positiveRe:             /article|body|content|entry|hentry|page|pagination|post|text/i,
+		negativeRe:             /combx|comment|contact|foot|footer|footnote|link|media|meta|promo|related|scroll|shoutbox|sponsor|tags|widget/i,
+		divToPElementsRe:       /<(a|blockquote|dl|div|img|ol|p|pre|table|ul)/i,
+		replaceBrsRe:           /(<br[^>]*>[ \n\r\t]*){2,}/gi,
+		replaceFontsRe:         /<(\/?)font[^>]*>/gi,
+		trimRe:                 /^\s+|\s+$/g,
+		normalizeRe:            /\s{2,}/g,
+		killBreaksRe:           /(<br\s*\/?>(\s|&nbsp;?)*){1,}/g,
+		videoRe:                /http:\/\/(www\.)?(youtube|vimeo)\.com/i
+  },
+
+	/**
+	 * Initialize a node with the readability object. Also checks the
+	 * className/id for special names to add to its score.
+	 *
+	 * @param Element
+	 * @return void
+	**/
+	initializeNode: function (node) {
+		node.readability = {"contentScore": 0};			
+
+		switch(node.tagName) {
+			case 'DIV':
+				node.readability.contentScore += 5;
+				break;
+
+			case 'PRE':
+			case 'TD':
+			case 'BLOCKQUOTE':
+				node.readability.contentScore += 3;
+				break;
+				
+			case 'ADDRESS':
+			case 'OL':
+			case 'UL':
+			case 'DL':
+			case 'DD':
+			case 'DT':
+			case 'LI':
+			case 'FORM':
+				node.readability.contentScore -= 3;
+				break;
+
+			case 'H1':
+			case 'H2':
+			case 'H3':
+			case 'H4':
+			case 'H5':
+			case 'H6':
+			case 'TH':
+				node.readability.contentScore -= 5;
+				break;
+		}
+
+		node.readability.contentScore += readability.getClassWeight(node);
+	},
+	
+	/***
+	 * grabArticle - Using a variety of metrics (content score, classname, element types), find the content that is
+	 *               most likely to be the stuff a user wants to read. Then return it wrapped up in a div.
+	 *
+	 * @return Element
+	**/
+	grabArticle: function (document_tab) {
+		var stripUnlikelyCandidates = readability.flagIsActive(readability.FLAG_STRIP_UNLIKELYS);
+
+		/**
+		 * First, node prepping. Trash nodes that look cruddy (like ones with the class name "comment", etc), and turn divs
+		 * into P tags where they have been used inappropriately (as in, where they contain no other block level elements.)
+		 *
+		 * Note: Assignment from index for performance. See http://www.peachpit.com/articles/article.aspx?p=31567&seqNum=5
+		 * TODO: Shouldn't this be a reverse traversal?
+		**/
+		for(var nodeIndex = 0; (node = document_tab.getElementsByTagName('*')[nodeIndex]); nodeIndex++)
+		{
+			/* Remove unlikely candidates */
+			if (stripUnlikelyCandidates) {
+				var unlikelyMatchString = node.className + node.id;
+				if (unlikelyMatchString.search(readability.regexps.unlikelyCandidatesRe) !== -1 &&
+				    unlikelyMatchString.search(readability.regexps.okMaybeItsACandidateRe) == -1 &&
+					node.tagName !== "BODY")
+				{
+					dbg("Removing unlikely candidate - " + unlikelyMatchString);
+					//node.parentNode.removeChild(node);
+          //nodeIndex--;
+          /* *** CANT REMOVE ELEMENTS FROM THE ORIGINAL PAGE *** */
+          $(node).addClass("removed_element");
+					continue;
+				}				
+			}
+
+			/* Turn all divs that don't have children block level elements into p's */
+			if (node.tagName === "DIV") {
+				if (node.innerHTML.search(readability.regexps.divToPElementsRe) === -1)	{
+					dbg("Altering div to p");
+					/*var newNode = document_tab.createElement('p');
+					try {
+						newNode.innerHTML = node.innerHTML;				
+						node.parentNode.replaceChild(newNode, node);
+						nodeIndex--;
+					}
+					catch(e)
+					{
+						dbg("Could not alter div to p, probably an IE restriction, reverting back to div.")
+          }*/
+          $(node).addClass("div_to_p");
+				}
+				else
+				{
+					/* EXPERIMENTAL */
+					for(var i = 0, il = node.childNodes.length; i < il; i++) {
+						var childNode = node.childNodes[i];
+						if(childNode.nodeType == 3) { // Node.TEXT_NODE
+							dbg("replacing text node with a p tag with the same content.");
+							var span = document_tab.createElement('span');
+							span.innerHTML = childNode.nodeValue;
+							span.style.display = 'inline';
+							span.className = 'readability-styled div_to_p';
+              childNode.parentNode.replaceChild(span, childNode);
+						}
+					}
+				}
+			} 
+		}
+
+		/**
+		 * Loop through all paragraphs, and assign a score to them based on how content-y they look.
+		 * Then add their score to their parent node.
+		 *
+		 * A score is determined by things like number of commas, class names, etc. Maybe eventually link density.
+		**/
+		var allParagraphs = $("p, div.div_to_p, span.div_to_p", document_tab);//document_tab.getElementsByTagName("p");
+		var candidates    = [];
+
+		for (var j=0; j	< allParagraphs.length; j++) {
+			var parentNode      = allParagraphs[j].parentNode;
+			var grandParentNode = parentNode.parentNode;
+			var innerText       = readability.getInnerText(allParagraphs[j]);
+
+			/* If this paragraph is less than 25 characters, don't even count it. */
+			if(innerText.length < 25)
+				continue;
+
+			/* Initialize readability data for the parent. */
+			if(typeof parentNode.readability == 'undefined')
+			{
+				readability.initializeNode(parentNode);
+				candidates.push(parentNode);
+			}
+
+			/* Initialize readability data for the grandparent. */
+			if(typeof grandParentNode.readability == 'undefined')
+			{
+				readability.initializeNode(grandParentNode);
+				candidates.push(grandParentNode);
+			}
+
+			var contentScore = 0;
+
+			/* Add a point for the paragraph itself as a base. */
+			contentScore++;
+
+			/* Add points for any commas within this paragraph */
+			contentScore += innerText.split(',').length;
+			
+			/* For every 100 characters in this paragraph, add another point. Up to 3 points. */
+			contentScore += Math.min(Math.floor(innerText.length / 100), 3);
+			
+			/* Add the score to the parent. The grandparent gets half. */
+			parentNode.readability.contentScore += contentScore;
+			grandParentNode.readability.contentScore += contentScore/2;
+		}
+
+		/**
+		 * After we've calculated scores, loop through all of the possible candidate nodes we found
+		 * and find the one with the highest score.
+		**/
+		var topCandidate = null;
+		for(var i=0, il=candidates.length; i < il; i++)
+		{
+			/**
+			 * Scale the final candidates score based on link density. Good content should have a
+			 * relatively small link density (5% or less) and be mostly unaffected by this operation.
+			**/
+			candidates[i].readability.contentScore = candidates[i].readability.contentScore * (1-readability.getLinkDensity(candidates[i]));
+
+			dbg('Candidate: ' + candidates[i] + " (" + candidates[i].className + ":" + candidates[i].id + ") with score " + candidates[i].readability.contentScore);
+
+			if(!topCandidate || candidates[i].readability.contentScore > topCandidate.readability.contentScore)
+				topCandidate = candidates[i];
+		}
+
+		/**
+		 * If we still have no top candidate, just use the body as a last resort.
+		 * We also have to copy the body node so it is something we can modify.
+		 **/
+		if (topCandidate == null || topCandidate.tagName == "BODY")
+		{
+			/*topCandidate = document_tab.createElement("DIV");
+			topCandidate.innerHTML = document_tab.body.innerHTML;
+			document_tab.body.innerHTML = "";
+      document_tab.body.appendChild(topCandidate);*/
+      topCandidate = document_tab.body;
+			readability.initializeNode(topCandidate);
+		}
+
+
+		/**
+		 * Now that we have the top candidate, look through its siblings for content that might also be related.
+		 * Things like preambles, content split by ads that we removed, etc.
+		**/
+		var articleContent        = document_tab.createElement("DIV");
+	        articleContent.id     = "readability-content";
+		var siblingScoreThreshold = Math.max(10, topCandidate.readability.contentScore * 0.2);
+		var siblingNodes          = topCandidate.parentNode.childNodes;
+		for(var i=0, il=siblingNodes.length; i < il; i++)
+		{
+			var siblingNode = siblingNodes[i];
+			var append      = false;
+
+			dbg("Looking at sibling node: " + siblingNode + " (" + siblingNode.className + ":" + siblingNode.id + ")" + ((typeof siblingNode.readability != 'undefined') ? (" with score " + siblingNode.readability.contentScore) : ''));
+			dbg("Sibling has score " + (siblingNode.readability ? siblingNode.readability.contentScore : 'Unknown'));
+
+			if(siblingNode === topCandidate)
+			{
+				append = true;
+			}
+			
+			if(typeof siblingNode.readability != 'undefined' && siblingNode.readability.contentScore >= siblingScoreThreshold)
+      {
+        /*append = true;*/
+        topCandidate = topCandidate.parentNode;
+			}
+			
+			if(siblingNode.nodeName == "P") {
+				var linkDensity = readability.getLinkDensity(siblingNode);
+				var nodeContent = readability.getInnerText(siblingNode);
+				var nodeLength  = nodeContent.length;
+				
+				if(nodeLength > 80 && linkDensity < 0.25)
+				{
+					//append = true;
+          topCandidate = topCandidate.parentNode;
+        }
+				else if(nodeLength < 80 && linkDensity == 0 && nodeContent.search(/\.( |$)/) !== -1)
+				{
+          //append = true;
+          topCandidate = topCandidate.parentNode;
+				}
+			}
+
+			if(append)
+			{
+				dbg("Appending node: " + siblingNode)
+
+				/* Append sibling and subtract from our list because it removes the node when you append to another node */
+        /*articleContent.appendChild(siblingNode);*/
+
+				i--;
+				il--;
+			}
+		}				
+		
+    //return articleContent;
+    return topCandidate;
+	},
+	
+	/**
+	 * Get the inner text of a node - cross browser compatibly.
+	 * This also strips out any excess whitespace to be found.
+	 *
+	 * @param Element
+	 * @return string
+	**/
+	getInnerText: function (e, normalizeSpaces) {
+		var textContent    = "";
+
+		normalizeSpaces = (typeof normalizeSpaces == 'undefined') ? true : normalizeSpaces;
+
+	  textContent = e.textContent.replace( readability.regexps.trimRe, "" );
+
+		if(normalizeSpaces)
+			return textContent.replace( readability.regexps.normalizeRe, " ");
+		else
+			return textContent;
+	},
+
+	/**
+	 * Get the density of links as a percentage of the content
+	 * This is the amount of text that is inside a link divided by the total text in the node.
+	 * 
+	 * @param Element
+	 * @return number (float)
+	**/
+	getLinkDensity: function (e) {
+		var links      = e.getElementsByTagName("a");
+		var textLength = readability.getInnerText(e).length;
+		var linkLength = 0;
+		for(var i=0, il=links.length; i<il;i++)
+		{
+			linkLength += readability.getInnerText(links[i]).length;
+		}		
+
+		return linkLength / textLength;
+	},
+	
+	/**
+	 * Get an elements class/id weight. Uses regular expressions to tell if this 
+	 * element looks good or bad.
+	 *
+	 * @param Element
+	 * @return number (Integer)
+	**/
+	getClassWeight: function (e) {
+		if(!readability.flagIsActive(readability.FLAG_WEIGHT_CLASSES)) {
+			return 0;
+		}
+
+		var weight = 0;
+
+		/* Look for a special classname */
+		if (e.className != "")
+		{
+			if(e.className.search(readability.regexps.negativeRe) !== -1)
+				weight -= 25;
+
+			if(e.className.search(readability.regexps.positiveRe) !== -1)
+				weight += 25;				
+		}
+
+		/* Look for a special ID */
+		if (typeof(e.id) == 'string' && e.id != "")
+		{
+			if(e.id.search(readability.regexps.negativeRe) !== -1)
+				weight -= 25;
+
+			if(e.id.search(readability.regexps.positiveRe) !== -1)
+				weight += 25;				
+		}
+
+		return weight;
+	},
+  flagIsActive: function(flag) {
+		return (readability.flags & flag) > 0;
+	}
+	
+};
